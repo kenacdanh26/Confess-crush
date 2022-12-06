93be691179eff48362a7b4d6a2b46b90b02bb440
kenacdanh26 <114002045+kenacdanh26@users.noreply.github.com>
2022-12-06 15:14:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: 'Send to my Trà Giang ',
-    introTitle: ' Hi cậu, đoán xem mình là ai nè ',
+    titleWeb: 'Send to my minhniemvui ',
+    introTitle: ' Hi anh, đoán xem em là ai nè ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^Đừng click mà^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Tớ kewt nhỉ <33',
+    btnYes: 'Em kewt nhỉ <33',
     btnNo: 'Khum hề nha :3',
-    question: 'Đoán được tớ nà ai chưa ạ? <3',
-    btnReply: 'Gửi cho tớ <3 ',
-    reply: 'Cậu là mụt người xinh đẹp ạ <3',
-    mess: 'Hehe tớ biết mà, iu nhaa😘😘',
-    messDesc: 'gặp nhau sớm nha ạaaaa',
-    btnAccept: 'Okiiiii luôn <3',
+    question: 'Đoán được em nà ai chưa ạ? <3',
+    btnReply: 'Gửi cho em <3 ',
+    reply: 'Em là mụt người xinh đẹp ạ <3',
+    mess: 'Hehe em biết mà, iu nhaa😘😘',
+    messDesc: 'Em thích anh lắm ạaaaa',
+    btnAccept: 'Anh cũm zậy á <3',
     messLink: 'https://www.facebook.com/hthanh.daver' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
