95fdfa3418c57620690218e5ed65867160c0bf1d
kenacdanh26 <114002045+kenacdanh26@users.noreply.github.com>
2022-12-06 15:25:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Send to my minhniemvui",
+    introTitle: 'Thái Bá Minh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnYes: 'Thích em lắm <33',
+    btnNo: 'Không thích đâu nha :3',
+    question: 'Biết sao em lại thích anh nhìu zậy khum? <3',
+    btnReply: 'Tại vì duyên đó hihi <3',
+    reply: 'Biết em là ai chưa z ạ? <33333333',
+    mess: 'Anh biết mà 🥰',
+    messDesc: 'Heheee chắc tới đây cũng biết rồi, ngại quáaa',
+    btnAccept: 'Send to meeee <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
