d758a8c02ef5e49469a46b67814fa58031efe5b9
kenacdanh26 <114002045+kenacdanh26@users.noreply.github.com>
2022-12-18 23:04:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Send to my minhniemvui",
+    introTitle: 'Thái Bá Minh',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    btnYes: 'Thích em lắm <33',
+    btnNo: 'Không không thích em nha :3',
+    question: 'Đoán ra em là ai chưa ạ? ^.^ ',
+    btnReply: 'Hoy chớt, chắc biết gòi',
+    reply: 'Em là một cô pé thích anh hoy',
+    mess: 'Anh biết mà😘',
+    messDesc: 'I am fall in luv w u',
+    btnAccept: 'so do I <3',
+    messLink: 'https://www.facebook.com/thaibaminh.dav' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
